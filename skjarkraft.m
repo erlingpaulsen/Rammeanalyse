0348dba79d20dcfb1d432074bd764ca2a89669bb
larsbow <larsbow@stud.ntnu.no>
2014-10-28 13:17:07 +0100

    Beskrivelse på skjarkraft.m

--- skjarkraft.m
+++ skjarkraft.m
@@ -1,4 +1,13 @@
 function skjarkraft = skjarkraft(last, nlast, endeM, nelem, elem, elementlengder)
+% Regner ut skjærkraften i hver ende i alle element i konstruksjonen.
+% last: matrise med alle laster i konstruksjonen.
+% nlast: antall laster i konstruksjonen
+% endeM: Matrise med alle endemomentene for alle elementer
+% nelem: Antall elementer i konstruksjonen.
+% elem: matrise med alle elementer i konstruksjonen.
+% elementlengder: Vektor med lengden til alle elementene.
+%
+% skjarkraft: (2 x nelem) matrise med endeskjærkraften til hvert element.
 skjarkraft = zeros(nelem, 2);
 q0 = q(nelem, elem, nlast, last, elementlengder);
 for i = 1:nelem;
